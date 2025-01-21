de7c454fdd17c78e8002268703f95d8830c7aac1
louis-ngugi <louisngugi858@gmail.com>
2025-01-22 01:07:14 +0300

    update App.js EyesOnMe.js Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,12 @@
-// Code Keypad Component Here
+// Code Keypad Component Hereimport React from "react";
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return <input type="password" onChange={handleChange} />;
 }
 
 export default Keypad;
